1bf3e792b7166abe64416357115f407d77e367de
Lattana <lattana@hellosezame.com>
2023-04-01 17:02:26 +0200

    [ADD] responsive

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/Button'
 export default function Example() {
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center py-12 px-6 sm:px-12">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
             Se connecter
