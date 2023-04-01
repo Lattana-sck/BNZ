e8d8116424af94221bb828d06310419793443d4b
Lattana <lattana@hellosezame.com>
2023-04-01 17:02:14 +0200

    [ADD] handling responsive

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -19,7 +19,7 @@ export function Footer() {
       <Container.Outer>
         <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex flex-col items-center justify-between gap-6 md:flex-row">
               <div className="hidden sm:flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/startups">Start-ups</NavLink>
