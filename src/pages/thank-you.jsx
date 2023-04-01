bebba1d2edbbf332ee506d4e4df6c5fa862dcad9
Lattana <lattana@hellosezame.com>
2023-04-01 15:04:23 +0200

    .

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -6,7 +6,7 @@ export default function ThankYou() {
   return (
     <>
       <Head>
-        <title>You’re subscribed - Spencer Sharp</title>
+        <title></title>
         <meta
           name="description"
           content="Thanks for subscribing to my newsletter."
