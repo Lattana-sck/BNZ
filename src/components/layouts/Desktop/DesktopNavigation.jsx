adf40fbd7b2886c4daca32afc0a0c1a7e64043a9
Lattana <lattana@hellosezame.com>
2023-04-29 14:42:36 +0200

    [ADD] marketplace page linked

--- src/components/layouts/Desktop/DesktopNavigation.jsx
+++ src/components/layouts/Desktop/DesktopNavigation.jsx
@@ -5,7 +5,6 @@ import React from 'react'
 
 function NavItem({ href, children }) {
   let isActive = useRouter().pathname === href
-
   return (
     <li className="whitespace-nowrap">
       <Link
@@ -18,14 +17,14 @@ function NavItem({ href, children }) {
         )}
       >
         {children}
-        {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-        )}
       </Link>
     </li>
   )
 }
+
 function DesktopNavigation(props) {
+  const isLoggin = true
+
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
@@ -35,8 +34,8 @@ function DesktopNavigation(props) {
         <NavItem href="/marketplace">Marketplace</NavItem>
         <NavItem href="/roadmap">Roadmap</NavItem>
         <NavItem href="/faq">FAQ & guides</NavItem>
-        <NavItem href="/register">Inscription</NavItem>
-        <NavItem href="/login">Connexion</NavItem>
+        {!isLoggin && <NavItem href="/register">Inscription</NavItem>}
+        {!isLoggin && <NavItem href="/login">Connexion</NavItem>}
       </ul>
     </nav>
   )
