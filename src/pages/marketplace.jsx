84da0de2e7163eb22682bcf2a141566d47b0c3bd
Lattana <lattana@hellosezame.com>
2023-04-29 18:23:16 +0200

    [ADD] grid nftcards

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -1,3 +1,4 @@
+import NftCards from '@/components/marketplace/Cards/NftCards'
 import Navbar from '@/components/marketplace/Navbar'
 import Head from 'next/head'
 import React from 'react'
@@ -9,10 +10,37 @@ function marketplace() {
         <title>BNZ - Marketplace</title>
         <meta name="description" content="BNZ - marketplace" />
       </Head>
-      <div className="p-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        {/* Content goes here */}
-      <Navbar />
+      <div className="mx-auto max-w-7xl p-6 sm:px-6 lg:px-8">
+        <Navbar />
+      </div>
+      <div className="mx-auto max-w-7xl p-6 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-6 gap-3 p-6">
+          <NftCards
+            title={'NFTparis'}
+            number={'#1203'}
+            description={'description du NFT'}
+            price={'0.1'}
+            currency={'ETH'}
+            lastSalePrice={'0.1'}
+          />
+          <NftCards
+            title={'NFTparis'}
+            number={'#1203'}
+            description={'description du NFT'}
+            price={'0.1'}
+            currency={'ETH'}
+            lastSalePrice={'0.1'}
+          />
+          <NftCards
+            title={'NFTparis'}
+            number={'#1203'}
+            description={'description du NFT'}
+            price={'0.1'}
+            currency={'ETH'}
+            lastSalePrice={'0.1'}
+          />
         </div>
+      </div>
     </>
   )
 }
